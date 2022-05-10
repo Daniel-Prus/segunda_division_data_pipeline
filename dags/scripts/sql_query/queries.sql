e78f7d257f1be6fe902f6ac719ec756cef12b2f8
Daniel-Prus <dlpruszynski@o2.pl>
2022-05-10 14:22:31 +0200

    query

--- dags/scripts/sql_query/queries.sql
+++ dags/scripts/sql_query/queries.sql
@@ -2,7 +2,7 @@
 
 SELECT vmr.fixture_date_utc, vmr.team_home_name, vmr.team_away_name, vmr.season, vmr.round, vmr.match_result
 FROM view_match_results AS vmr
-WHERE league_id = 141 AND season = get_current_season(141) AND round = get_next_round(141)
+WHERE league_id = 141 AND season = get_current_season(141) AND round = get_current_round(141)
 
 
 -- get next round segdiv
@@ -17,3 +17,4 @@ WHERE league_id = 141 AND season = get_current_season(141) AND round = get_next_
 select team_position, team_name, mp, w, d, l, gf, ga, gd, pts
 from mat_view_segdiv_current_standings
 where standings_type_id = 1
+ORDER BY team_position
