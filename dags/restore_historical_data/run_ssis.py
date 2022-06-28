f3816638f529dc1ce0f0b901f743ec37a86dfbfe
Daniel-Prus <dlpruszynski@o2.pl>
2022-06-28 15:15:55 +0200

    SSIS

--- dags/restore_historical_data/run_ssis.py
+++ dags/restore_historical_data/run_ssis.py
@@ -2,9 +2,9 @@ import subprocess
 from datetime import datetime
 
 # run power shell dtexec
-run_dtexec_script = r"dtexec /File 'csv_to_postgres_ssis\csv_to_postgres_ssis\restore_data_football_raw_db.dtsx'"
+run_dtexec_script = r"dtexec /File csv_to_postgres_ssis\csv_to_postgres_ssis\restore_data_football_raw_db.dtsx"
 
-run_detexec_SSIS = subprocess.run(run_dtexec_script, shell=True, text=True)
+run_detexec_SSIS = subprocess.run(run_dtexec_script, capture_output=True, text=True)
 
 stdout = run_detexec_SSIS.stdout
 
@@ -18,4 +18,4 @@ with open(f"csv_to_postgres_ssis/logs/ssis_output_{dt_string}.txt", 'w') as file
 
 # open SSIS output
 output_script = f"notepad csv_to_postgres_ssis/logs/ssis_output_{dt_string}.txt"
-subprocess.run(output_script, shell=True, text=True)
+subprocess.run(output_script)
